621a87ba14c8643b76de1a2b6ca353a326e907f2
=Sumit Choudhary <choudharysumit150@gmail.com>
2019-08-04 17:53:01 +0530

    Clark Codility Problem

--- CodilityExam/Program.cs
+++ CodilityExam/Program.cs
@@ -10,6 +10,13 @@ namespace CodilityExam
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Codility Clark Test Started..");
+            int[] array = {5,2,6,8,1,7 };
+            int aliceTree = 3;
+            int bobtrees = 2;
+            int result = Solution.solution(array,aliceTree,bobtrees);
+            Console.WriteLine("Maximum Apples can be collected :" + result);
+            Console.ReadLine();
         }
     }
 }
---
+++ CodilityExam/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityExam
+{
+    public static class Solution
+    {
+        public static int solution(int[] A, int K, int L)
+        {
+            int maxmimumApples = 0;
+            int noOfTrees = A.Length;
+
+            if ((K + L) > noOfTrees)
+            {
+                return -1;
+            }
+            int maxAppleAlice = 0;
+            int maxAppleBob = 0;
+            for (int i = 0; i < noOfTrees; i++)
+            {
+
+            }
+            return maxmimumApples;
+        }
+    }
+}
